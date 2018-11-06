1d0e4a8af8773e9baa38d2ab15f08abf236467c0
Judy <yanghuanghuang@gmail.com>
2018-11-06 14:29:58 -0800

    Handle the 404 page

--- src/app.js
+++ src/app.js
@@ -1,19 +1,31 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch, Link } from 'react-router-dom'
 import SearchBook from './searchBook'
 import Homepage from './homepage'
 import './app.css'
+
+
+const NoMatch = ({ location }) => (
+  <div>
+    <h3> Sorry, we can't found this page: { location.pathname }.</h3>
+    <p> Go back to the <Link to='/'> Homepage </Link></p>
+  </div>
+)
+
 
 class BooksApp extends React.Component {
   render () {
     return (
       <div className='app'>
-        <Route exact path='/' render={() => (
-          <Homepage />
-        )} />
-        <Route path='/search' render={() => (
-          <SearchBook />
-        )} />
+        <Switch>
+          <Route exact path='/' render={() => (
+            <Homepage />
+          )} />
+          <Route path='/search' render={() => (
+            <SearchBook />
+          )} />
+          <Route component={ NoMatch } />
+        </Switch>
       </div>
     )
   }
